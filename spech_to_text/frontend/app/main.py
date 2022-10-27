3e639b149f154ca6d3d2f681f19c4cbbc0c9675f
Andrey.Torshin <dchorch@gmail.com>
2022-10-27 05:04:29 -0700

    add speech_to_text not recasepunc make front

---
+++ spech_to_text/frontend/app/main.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from vosk import Model, KaldiRecognizer, SetLogLevel
+from pydub import AudioSegment
+import json
+import os
+
+import requests
+
+# Настройка боковой панели
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    This service for recognition and processing audio to text.
+    """
+)
+st.sidebar.info("Feel free to collaborate and comment on the work. The github link can be found "
+                "(https://github.com/")
+
+st.header("Trascribe Audio, only mp3 format!")
+fileObject = st.file_uploader(label="Please upload your file")
+
+if st.button("File Transfer"):
+    #if image is not None and style is not None:
+    files = {"file": fileObject.getvalue()}
+    res = requests.post(f"http://51.250.66.35:8081/upload", files=files)
+    img_path = res.json()
+    if img_path != None:
+        st.text("File upload to back- OK! Processing.....")
+'''
+if fileObject:
+    st.text("Processing: ")
+
+    # Получаем обьект файла
+    bytes_data = fileObject.getvalue()
+    f = open("soung.mp3", "wb")
+    f.write(bytes_data)
+
+    # Обрезаем файл до 30 сек
+    file_name = 'soung'
+
+    startMin = 0
+    startSec = 0
+    endMin = 0
+    endSec = 60
+
+    # Time to miliseconds
+    startTime = startMin * 60 * 1000 + startSec * 1000
+    endTime = endMin * 60 * 1000 + endSec * 1000
+
+    # Opening file and extracting segment
+    song = AudioSegment.from_mp3(file_name + '.mp3')
+    extract = song[startTime:endTime]
+
+    # Saving
+    extract.export(file_name + '-extract.mp3', format="mp3")
+    with st.spinner('Wait for it...'):
+        SetLogLevel(0)
+
+        # Проверяем наличие модели
+        if not os.path.exists("model"):
+            print(
+                "Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+            exit(1)
+
+        # Устанавливаем Frame Rate
+        FRAME_RATE = 16000
+        CHANNELS = 1
+
+        model = Model("model")
+        rec = KaldiRecognizer(model, FRAME_RATE)
+        rec.SetWords(True)
+
+        # Используя библиотеку pydub делаем предобработку аудио
+        mp3 = AudioSegment.from_mp3("soung-extract.mp3")
+        mp3 = mp3.set_channels(CHANNELS)
+        mp3 = mp3.set_frame_rate(FRAME_RATE)
+
+        # Преобразуем вывод в json
+        rec.AcceptWaveform(mp3.raw_data)
+        result = rec.Result()
+        text = json.loads(result)["text"]
+
+        #cased = subprocess.check_output('python3 recasepunc/recasepunc.py predict recasepunc/checkpoint', shell=True,
+        ##                               text=True, input=text)
+
+        audio_file = open('soung-extract.mp3', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio / ogg')
+        audio_file.close()
+
+        st.subheader("Trascribe result: ")
+        st.markdown(text)
+        f.close()
+        os.remove("soung.mp3")
+        os.remove("soung-extract.mp3")
+
+    st.success('Done!')
+'''
