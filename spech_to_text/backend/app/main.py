a6053343d2c29e3258721c01fdd3d23e6eded1f4
Andrey.Torshin <dchorch@gmail.com>
2022-10-28 03:20:52 -0700

    add speech_to_text not recasepunc make back save to disk extract -OK

--- spech_to_text/backend/app/main.py
+++ spech_to_text/backend/app/main.py
@@ -42,7 +42,30 @@ async def post_endpoint(file: bytes = File(...)):
         # Saving extract
         extract.export('./save/extract.mp3', format="mp3")
 
-    #return {"name": content}
+        #NLP processing
+        SetLogLevel(0)
+        if not os.path.exists("model"):
+            print(
+                "Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+            exit(1)
+
+        # Устанавливаем Frame Rate
+        FRAME_RATE = 16000
+        CHANNELS = 1
+
+        model = Model("model")
+        rec = KaldiRecognizer(model, FRAME_RATE)
+        rec.SetWords(True)
+
+        # Используя библиотеку pydub делаем предобработку аудио
+        mp3 = AudioSegment.from_mp3("extract.mp3")
+        mp3 = mp3.set_channels(CHANNELS)
+        mp3 = mp3.set_frame_rate(FRAME_RATE)
+
+        rec.AcceptWaveform(mp3.raw_data)
+        result = rec.Result()
+        text = json.loads(result)["text"]
+    return {"name": text}
 
 
 if __name__ == "__main__":
