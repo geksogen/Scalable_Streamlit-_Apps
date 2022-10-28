29f2eec46287bbcba0baf9d928c357e84adc2cb2
Andrey.Torshin <dchorch@gmail.com>
2022-10-28 05:46:35 -0700

    add speech_to_text not recasepunc final test -OK

--- spech_to_text/backend/app/main.py
+++ spech_to_text/backend/app/main.py
@@ -27,7 +27,7 @@ async def post_endpoint(file: bytes = File(...)):
 
         # Time to miliseconds
         startTime = startMin * 60 * 1000 + startSec * 1000
-        endTime = endMin * 60 * 9000 + endSec * 9000
+        endTime = endMin * 60 * 10000 + endSec * 10000
 
         # Opening file and extracting segment
         song = AudioSegment.from_mp3('./save/soung.mp3')
