8796a3f2fdb9c59c6e9f9c52ec6bc4f2a21644a6
Andrey.Torshin <dchorch@gmail.com>
2022-10-31 02:09:20 -0700

    add speech_to_text not recasepunc Dockerfile

--- spech_to_text/README.md
+++ spech_to_text/README.md
@@ -1,1 +1,3 @@
 https://python.astrotech.io/fastapi/fastapi/http-files.html
+
+
--- spech_to_text/backend/app/main.py
+++ spech_to_text/backend/app/main.py
@@ -64,5 +64,5 @@ async def post_endpoint(style: str, file: bytes = File(...)):
 
     return {"name": text}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="0.0.0.0", port=8081)
