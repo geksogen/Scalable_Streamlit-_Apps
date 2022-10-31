78fdfa1e37547ff0fc5aaa75c208d376e59cbc19
Andrey.Torshin <dchorch@gmail.com>
2022-10-31 06:10:17 -0700

    add speech_to_text not recasepunc no Dockerfile

--- spech_to_text/backend/app/main.py
+++ spech_to_text/backend/app/main.py
@@ -10,15 +10,15 @@ import config
 
 app = FastAPI()
 
-#@app.get("/")
-#def read_root():
-#    return {"message": "Welcome from the API"}
+@app.get("/")
+def read_root():
+    return {"message": "Welcome from the API"}
 
 @app.post("/{style}")
 async def post_endpoint(style: str, file: bytes = File(...)):
 
     #Save Upload file to disk s
-    async with aiofiles.open('./tmp/save/soung.mp3', 'wb') as out_file:
+    async with aiofiles.open('./save/soung.mp3', 'wb') as out_file:
         await out_file.write(file)  # async write
 
         startMin = 0
@@ -31,11 +31,11 @@ async def post_endpoint(style: str, file: bytes = File(...)):
         endTime = endMin * 60 * 10000 + endSec * 10000
 
         # Opening file and extracting segment
-        song = AudioSegment.from_mp3('./tmp/save/soung.mp3')
+        song = AudioSegment.from_mp3('./save/soung.mp3')
         extract = song[startTime:endTime]
 
         # Saving extract
-        extract.export('./tmp/save/extract.mp3', format="mp3")
+        extract.export('./save/extract.mp3', format="mp3")
 
         # NLP processing
         SetLogLevel(0)
@@ -64,5 +64,5 @@ async def post_endpoint(style: str, file: bytes = File(...)):
 
     return {"name": text}
 
-#if __name__ == "__main__":
-#    uvicorn.run("main:app", host="0.0.0.0", port=8081)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8081)
