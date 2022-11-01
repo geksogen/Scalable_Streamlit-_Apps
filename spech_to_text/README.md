09a5a8f7790c9de320d4f2889aa56dba90269b06
Andrey.Torshin <dchorch@gmail.com>
2022-11-01 00:24:21 -0700

    add speech_to_text not recasepunc prepare_models.sh

--- spech_to_text/README.md
+++ spech_to_text/README.md
@@ -3,6 +3,10 @@
 ```BASH
 git clone https://github.com/geksogen/Scalable_Streamlit-_Apps.git
 cd backend/app
+mkdir punctuation_model
+mkdir model_big
+mkdir model_smail
+
 
 
 
--- spech_to_text/backend/app/prepare_models.sh
+++ spech_to_text/backend/app/prepare_models.sh
@@ -13,5 +13,5 @@ rm -rf model.zip
 cd ../model_smail
 curl -o ./model.zip https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip
 unzip model.zip
-mv vosk-model-ru-0.22/ model
+mv vosk-model-small-ru-0.22/ model
 rm -rf model.zip
