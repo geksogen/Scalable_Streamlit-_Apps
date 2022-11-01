c0511c3f9daf2c9848d23077a59b4e84afa2f38c
Andrey.Torshin <dchorch@gmail.com>
2022-11-01 03:22:06 -0700

    initial comit

--- spech_to_text/frontend/app/main.py
+++ spech_to_text/frontend/app/main.py
@@ -42,9 +42,4 @@ if st.button("Transcription"):
         # Save and download transcribe text
         st.download_button('Download file', img_path.get("name"))
 
-        #with open('../../backend/app/save/text.txt') as f:
-        #    st.download_button('Download text', f)
-
-        #if st.download_button(...):
-        #    st.write('Thanks for downloading!')
     st.success('Done!')
