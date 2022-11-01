7abf5004e37a1ed078e27fbb3a7dcb08d3985ed5
Andrey.Torshin <dchorch@gmail.com>
2022-11-01 01:21:25 -0700

    add speech_to_text not recasepunc prepare_models.sh

--- spech_to_text/backend/app/main.py
+++ spech_to_text/backend/app/main.py
@@ -65,4 +65,4 @@ async def post_endpoint(style: str, file: bytes = File(...)):
     return {"name": text}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081)
+    uvicorn.run("main:app", host="178.154.240.231", port=8081)
